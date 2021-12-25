3b26054d37a64839691c675c4d81e35bb6fd6cc9
tshibata <staatsschreiber@gmail.com>
2021-12-25 14:22:15 +0900

    Fix default schedule for backup

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -64,7 +64,7 @@ public class DemoApplication {
 	@Value("${app.backup.file:'backup.mv.db'}")
 	String backup;
 
-	@Scheduled(cron = "${app.backup.cron:'-'}")
+	@Scheduled(cron = "${app.backup.cron:-}")
 	void dbCron() throws SQLException, IOException {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(backup);
 		server.stop();
