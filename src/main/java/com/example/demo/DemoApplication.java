9fa54c880b4807109b3ae197247f0124a2498a22
tshibata <staatsschreiber@gmail.com>
2022-01-16 17:49:40 +0900

    Don't start H2 server

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -21,61 +21,23 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.handler.MappedInterceptor;
 
-import org.h2.tools.Server;
-
 @SpringBootApplication
 @EnableScheduling
 public class DemoApplication {
 
-	private static Server server;
-
-	private static String standby;
-
 	private static ConfigurableApplicationContext context;
 
 	public static void main(String[] args) throws SQLException, IOException {
-		if (args.length == 1) {
-			try {
-				standby = args[0];
-				if (Files.exists(Paths.get(standby))) {
-					Files.copy(Paths.get(standby), Paths.get("h2.mv.db"), StandardCopyOption.REPLACE_EXISTING);
-				}
-			} catch (IOException ex) {
-				// should retry?
-			}
-		}
-		server = Server.createTcpServer("-ifNotExists").start();
 		context = SpringApplication.run(DemoApplication.class, args);
 	}
 
 	public static void shutdown() {
 		context.close();
-		server.stop();
-		if (standby != null) {
-			try {
-				Files.copy(Paths.get("h2.mv.db"), Paths.get(standby), StandardCopyOption.REPLACE_EXISTING);
-			} catch (IOException ex) {
-				// should retry?
-			}
-		}
 		System.exit(0);
-	}
-
-	@Value("${app.backup.file:'backup.mv.db'}")
-	String backup;
-
-	@Scheduled(cron = "${app.backup.cron:-}")
-	void dbCron() throws SQLException, IOException {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(backup);
-		server.stop();
-		String name = LocalDateTime.now().format(formatter);
-		Files.copy(Paths.get("h2.mv.db"), Paths.get(name), StandardCopyOption.REPLACE_EXISTING);
-		server = Server.createTcpServer().start();
 	}
 
 	@Bean
 	PasswordEncoder encoder() {
-
 		return new BCryptPasswordEncoder();
 	}
 
