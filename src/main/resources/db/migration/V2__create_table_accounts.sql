65aabd8876c22ad21d6bf657c8506d7c3da6f477
tshibata <staatsschreiber@gmail.com>
2022-11-26 15:57:59 +0900

    Give name to unique constraint

--- src/main/resources/db/migration/V2__create_table_accounts.sql
+++ src/main/resources/db/migration/V2__create_table_accounts.sql
@@ -1,9 +1,10 @@
 CREATE TABLE accounts (
 	id BIGSERIAL,
-	username VARCHAR(15) UNIQUE NOT NULL,
+	username VARCHAR(15) NOT NULL,
 	password VARCHAR(60) NOT NULL,
 	description VARCHAR(1000) NOT NULL,
 	valid BOOLEAN NOT NULL,
+	CONSTRAINT account_username_uniq UNIQUE (username),
 	PRIMARY KEY (id)
 );
 CREATE INDEX accounts_by_username ON accounts (username);
