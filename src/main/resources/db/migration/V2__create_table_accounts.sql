da01bd98cbeca4b2b8360421cae8f0503b9d6a5c
tshibata <staatsschreiber@gmail.com>
2022-12-08 21:24:40 +0900

    Simplify the account table

--- src/main/resources/db/migration/V2__create_table_accounts.sql
+++ src/main/resources/db/migration/V2__create_table_accounts.sql
@@ -4,9 +4,8 @@ CREATE TABLE accounts (
 	password VARCHAR(60) NOT NULL,
 	description VARCHAR(1000) NOT NULL,
 	valid BOOLEAN NOT NULL,
-	CONSTRAINT account_username_uniq UNIQUE (username),
 	PRIMARY KEY (id)
 );
-CREATE INDEX accounts_by_username ON accounts (username);
+CREATE UNIQUE INDEX accounts_by_username ON accounts (username);
 CREATE INDEX accounts_by_valid_and_id ON accounts (valid, id);
 
