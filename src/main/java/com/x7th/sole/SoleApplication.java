897fb1ffe8e04485aaa042d655a4f1cd0f33d33c
tshibata <staatsschreiber@gmail.com>
2023-01-28 10:28:43 +0900

    Clean up a bit

--- src/main/java/com/x7th/sole/SoleApplication.java
+++ src/main/java/com/x7th/sole/SoleApplication.java
@@ -3,8 +3,6 @@ package com.x7th.sole;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.handler.MappedInterceptor;
